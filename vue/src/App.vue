d73cd4ec51497329c123d03305ac21d6d9add36a
hashedtomato3 <hashedtomato3@gmail.com>
2021-10-09 08:53:16 +0000

    test

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -91,7 +91,7 @@
                   <b-field label="Menu Items"></b-field>                      
                 </div>
                 <div class="level-left" style="margin:0px">
-                  <b-button type="is-primary" inverted v-on:click="onclick_add">
+                  <b-button type="is-primary" inverted v-on:click="onclick_add" title="Add Item">
                     <div style="display:flex">
                       <svg style="width:24px;height:24px;" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M17,13H13V17H11V13H7V11H11V7H13V11H17M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" />
